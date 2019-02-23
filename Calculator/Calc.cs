d30cadd22855a4d7d45c22b3ce473e9c730d690f
AnastasiaKryven <36449168+AnastasiaKryven@users.noreply.github.com>
2019-02-23 13:31:57 +0200

    proj

---
+++ Calculator/Calc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calc : ICalculate
+    {
+        public double Division(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Multiplication(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Subtraction(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Sum(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Calculator/ICalculate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public interface ICalculate
+    {
+        double Sum(double a, double b);
+        double Subtraction(double a, double b);
+        double Division(double a, double b);
+        double Multiplication(double a, double b);
+    }
+}
